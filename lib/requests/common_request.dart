29769eb0ff4ca442726e73a8cf322c91d59c246e
xiaoyaocz <xiaoyaocz@52uwp.com>
2022-12-12 16:03:02 +0800

    更新版本信息

--- lib/requests/common_request.dart
+++ lib/requests/common_request.dart
@@ -6,7 +6,7 @@ class CommonRequest {
   /// 检查更新
   Future<VersionModel> checkUpdate() async {
     var result = await Dio().get(
-      "https://cdn.jsdelivr.net/gh/xiaoyaocz/flutter_cnblogs@master/document/version.json",
+      "https://cdn.jsdelivr.net/gh/xiaoyaocz/flutter_cnblogs@master/document/new_version.json",
       queryParameters: {
         "ts": DateTime.now().millisecondsSinceEpoch,
       },
